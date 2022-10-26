f57b93392acb6789d33b0e9936cbf55e16eed1af
DjBpF <bpfcomp2005@gmail.com>
2022-10-26 21:46:27 +0300

    added project files

---
+++ project/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello there!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/google85/docker-react-dev"
+          target="_blank" rel="noopener noreferrer"
+        >
+          GitHub repo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
